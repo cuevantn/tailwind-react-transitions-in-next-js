689887a8b0fe5ab389d4258e6eceb6c2fcc5afca
Anthony Cueva <4nthonycuev4@gmail.com>
2023-01-08 20:10:34 -0500

    repo name

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Animating buttons onClick with Tailwind CSS</title>
+      <title> Tailwind CSS and React Transitions in Next.js</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="A blog by cuevatnt" />
       <link rel="icon" href="/favicon.ico" />
